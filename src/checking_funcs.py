eb24764f8e4e80efcfcd7599957d3d0e496283b4
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-07-26 15:59:37 +0200

    Add column and box checks.

--- src/checking_funcs.py
+++ src/checking_funcs.py
@@ -1,16 +1,25 @@
 def sep_row(board, pos_idx):
     row_idx = pos_idx // 9
-    return board[row_idx*9: row_idx*9 + 9]
+    for i in range(row_idx*9,row_idx*9 + 9):
+        yield board[i]
 
 def row_check(board ,pos_idx ,value):
     return not any(value == i for i in sep_row(board, pos_idx))
 
 def sep_col(board, pos_idx):
     col_idx = pos_idx % 9
-    return (board[i*9 + col_idx] for i in range(9))
+    for i in range(9):
+        yield board[i*9 + col_idx]
 
 def col_check(board ,pos_idx ,value):
-    ...
+    return not any(value == i for i in sep_col(board, pos_idx))
+
+def sep_box(board, pos_idx):
+    row_idx = pos_idx // 9
+    col_idx = pos_idx % 9
+    for i in range(3):
+        for j in range(3):
+            yield board[(row_idx+i)*9 + col_idx + j]
 
 def box_check():
     ...
--- tests/test_checking_fucns.py
+++ tests/test_checking_fucns.py
@@ -1,4 +1,4 @@
-from src.checking_funcs import sep_row, row_check, col_check, box_check
+from src.checking_funcs import sep_row, row_check, sep_col, col_check, sep_box, box_check
 
 board = [0,0,0,0,0,0,0,0,0,
          0,0,0,0,0,0,0,0,0,
@@ -9,36 +9,25 @@ board = [0,0,0,0,0,0,0,0,0,
          0,0,0,0,0,0,0,0,0,
          0,0,0,0,0,0,0,0,0,
          0,0,0,0,0,0,0,0,0,]
-def test_sep_row():
-    board = [0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             1,2,3,4,5,6,7,8,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,3,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,]
-    
-    assert sep_row(board, 18) == [1,2,3,4,5,6,7,8,0]
-    assert sep_row(board, 26) == [1,2,3,4,5,6,7,8,0]
 
-    assert sep_row(board, 45) == [0,0,3,0,0,0,0,0,0]
-    assert sep_row(board, 53) == [0,0,3,0,0,0,0,0,0]
+def test_sep_row():
+    
+    assert list(sep_row(board, 18)) == [1,2,3,4,5,6,7,8,0]
+    assert list(sep_row(board, 26)) == [1,2,3,4,5,6,7,8,0]
+
+    assert list(sep_row(board, 45)) == [0,0,3,0,0,0,0,0,0]
+    assert list(sep_row(board, 53)) == [0,0,3,0,0,0,0,0,0]
 
 def test_row_check():
-    board = [0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             1,2,3,4,5,6,7,8,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,3,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,0,]
 
     assert row_check(board, 26, 9)
     assert not row_check(board, 26, 8)
 
     assert row_check(board, 45, 4)
     assert not row_check(board, 45, 3)
+
+def test_sep_col():
+    assert list(sep_col(board, 2)) == [0, 0, 3, 0, 0, 3, 0, 0, 0]
+
+def test_sep_box():
+    assert list(sep_box(board, 0)) == [0,0,0,0,0,0,1,2,3]
